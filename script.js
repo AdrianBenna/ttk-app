8c038436e679b81b10e7a0514d02a3dc95d5612a
Adrian Benna <adrianbenna@gmail.com>
2021-01-25 13:14:43 -0800

    Weapon Name Pull
    input name passes to p tag

--- script.js
+++ script.js
@@ -1,43 +1,3 @@
-/*
-var weaponDamage = document.getElementById("damage");
-var roundsPerMin = document.getElementById("rpm");
-var playerHealth = document.getElementById("health");
-
-
-function getDamage(yip){
-        document.getElementById("damage").innerText = yip;
-     return document.getElementById("output").innerText = yip;
-}
-
-function getRounds(){
-    return document.getElementById("rpm").innerText;
-}
-
-function getHealth(){
-    return document.getElementById("health").innerText;
-}
-
-
-  function myFunction() {
-    var x = document.getElementById("damage").value;
-    document.getElementById("output").innerHTML = x;
-  }
-
-  function myFunctionAA() {
-    var y = document.getElementById("rpm").value;
-    document.getElementById("output1").innerHTML = y;
-  }
-
-  function myFunctionBB() {
-    var z = document.getElementById("health").value;
-    document.getElementById("output2").innerHTML = z;
-  }
-
-  document.getElementById("calculate").addEventListener("click", myFunction);
-  document.getElementById("calculate").addEventListener("click", myFunctionAA);
-  document.getElementById("calculate").addEventListener("click", myFunctionBB);
-
-*/
 var shots = 0;
 
 function shotsToKill() {
@@ -45,10 +5,7 @@ function shotsToKill() {
     var playerHealth = document.getElementById("health").value;
     shots = Math.ceil(playerHealth / weaponDamage);
     document.getElementById("output").innerHTML = shots + " shots to kill";
-    
 }
-
-
 
 function ttk() {
     var roundsPerMin = document.getElementById("rpm").value;
@@ -60,8 +17,12 @@ function ttk() {
     window.location.reload();
   }
   
+  function namePull(){
+    document.getElementById("outputName").innerText = document.getElementById("inputName").value;
+  }
 
-
+  
+  document.getElementById("calculate").addEventListener("click", namePull);
   document.getElementById("calculate").addEventListener("click", shotsToKill);
   document.getElementById("calculate").addEventListener("click", ttk);
   document.getElementById("reset").addEventListener("click", resetPage);
